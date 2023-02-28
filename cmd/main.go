92820d68d21ec1ba2d99fc7cd476b3e458f43ada
Captain <captainali716gmail.com>
2023-03-01 01:23:57 +0500

    aa

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+	"github.com/zhashkevych/todo-app"
+	"github.com/zhashkevych/todo-app/pkg/handler"
+	"github.com/zhashkevych/todo-app/pkg/repository"
+	"github.com/zhashkevych/todo-app/pkg/service"
+)
+
+func main() {
+	if err:= initConfig(); err != nil {
+		log.Fatalf("error initializating configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
